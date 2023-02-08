ba411087f171cf452d24449f27f747bd67747731
Seanghor <91684339+Seanghor@users.noreply.github.com>
2023-02-08 14:51:23 +0700

    Update Home.jsx
    revert home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -99,3 +99,88 @@ const Home = () => {
           </div>
         </div>
        </div>
+{/* Container Section Start */}
+        <section className="bg-color">
+          <div className="">
+            <div className="row justify-content-center">
+             
+              <Cards 
+                img={Bin} 
+                name="Bin"
+                newPrice="30.00"
+                oldPrice="25.00"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+              <Cards 
+                img={CuttingBoard} 
+                name="Cutting Board"
+                newPrice="8.00"
+                oldPrice="15.00"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+              <Cards 
+                img={Pan2} 
+                name="Mit"
+                newPrice="10.00"
+                oldPrice="15.00"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+            </div>
+
+            {/* Top Picks For You Start */}
+            <div className="title">
+              <h2>
+                <span>Top Picks For You</span>
+              </h2>
+              <p>
+                Find a bright ideal to suit your taste with our great selection
+                of suspension, florr and table lights.
+              </p>
+            </div>
+            {/* Top Picks For You End */}
+
+            {/* Product Card Start */}
+            <Grid className="row">
+              <ProductCard
+                img={Pan}
+                name="Pan"
+                newPrice="10.00"
+                oldPrice="15.00"
+                discount=" 15"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+              <ProductCard
+                img={Bowl}
+                name="Bowl"
+                newPrice="4.00"
+                oldPrice="9.00"
+                discount=" 15"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+              <ProductCard
+                img={Microwave}
+                name="Microwave"
+                newPrice="81.00"
+                oldPrice="89.00"
+                discount=" 15"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+              <ProductCard
+                img={Pot}
+                name="Pot"
+                newPrice="15.00"
+                oldPrice="19.00"
+                discount=" 15"
+                desc="Moving into a new house or buying your first home??Kit out your kitchen with this kitchen utensils list containing 46 of the most essential items!"
+              />
+            </Grid>
+            {/* Product Card End */}
+          </div>
+        </section>{" "}
+        {/* Container Section End */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
