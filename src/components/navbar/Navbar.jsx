7e1a4040aac8f1c995f9a094e6d6b08caf3972cb
Seanghor <91684339+Seanghor@users.noreply.github.com>
2023-02-08 14:53:42 +0700

    Update Navbar.jsx
    revert navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./navbar.css";
-import Pan from "../.././assets/image/pan/pan5-removebg.png";
 import kitchiLogo from "../.././assets/image/logo.svg";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
@@ -9,12 +8,21 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-light py-0">
         <div className="container-fluid">
           <div className="logo">
-            <img src={kitchiLogo} alt="" className="kitchilogo" />
+            <img src={kitchiLogo} alt=""  className="kitchilogo"/>
           </div>
-          <div
-            className="collapse navbar-collapse justify-content-center "
-            id="navbarSupportedContent"
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-center " id="navbarSupportedContent">
             <ul className="navbar-nav mb-1 mb-lg-0 ml-xl-5 ml-sm-5">
               <NavLink className="nav-item" to="/">
                 <li className="nav-link" aria-current="page" href="/">
@@ -37,65 +45,30 @@ const Navbar = () => {
                 </li>
               </NavLink>
             </ul>
+           
           </div>
-          <div
-            className="collapse navbar-collapse pr-xl-5 "
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/signin">
-                  <i className="bi bi-person-exclamation icon"></i>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
+          <div className="collapse navbar-collapse pr-xl-5 "  id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/signin">
+                    <i className="bi bi-person-exclamation icon"></i>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
                   <i className="bi bi-heart icon"></i>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/cart">
-                  <i className="bi bi-cart icon"></i>
-                  <span class="badge" id="lblCartCount">
-                    {" "}
-                    5{" "}
-                  </span>
-                </NavLink>
-              </li>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/cart">
+                    <i className="bi bi-cart icon"></i>
+                    <span class='badge' id='lblCartCount'> 5 </span>
+                  </NavLink>
+                </li>
             </ul>
           </div>
         </div>
       </nav>
-      <div className="row" id="Bg">
-        <div className="col-md-6">
-          <div className="card" id="card1">
-            <div className="model">
-              <h3>New Model</h3>
-              <NavLink className="btn-learn-more" to="/shop">
-                <li className="nav-link" href="/shop">
-                  Shop Now
-                </li>
-              </NavLink>
-              <h4>
-                Anodized to absorb, distribute and transfer
-                <br /> heat evenly and efficiently, these two frying
-                <br /> pans sautée mushrooms, fry eggs, caramelize
-                <br /> onions and bake frittatas.
-              </h4>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-6">
-          <div className="card" id="card">
-            <div className="model">
-              <div className="">
-                <img src={Pan} alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
